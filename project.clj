18ad9e2a07e6ce29757ab9fc08f9fa3fa7eeb62d
spike <bank.tellr@gmail.com>
2023-02-10 20:29:57 +0900

    Updates build settings

--- project.clj
+++ project.clj
@@ -8,9 +8,51 @@
                  "src/cljc"
                  "src/cljs"]
 
-  :dependencies [[org.clojure/clojure "1.11.1"]
-                 [seesaw "1.5.0"]]
+  :dependencies [[org.clojure/clojure "1.11.1"]             ; Server
+                 [ring/ring-core "1.9.5"]
+                 [ring/ring-jetty-adapter "1.9.5"]
+                 [seesaw "1.5.0"]                           ; > Head
+                 [org.clojure/clojurescript "1.11.57"]      ; Client/Controller
+                 [compojure "1.7.0"]
+                 [hiccup "1.0.5"]
+                 [domina "1.0.3"]
+                 [cljsjs/react "17.0.2-0"]
+                 [cljsjs/react-dom "17.0.2-0"]
+                 [reagent "1.1.1"]]
 
-  :aliases {"gui-start" ["run" "-m" "gui.core"]}
+  :plugins [[lein-cljsbuild "1.1.8"]]
+
+  :cljsbuild {:builds
+              {:dev
+               {:source-paths ["src/cljs"]
+                :compiler {:output-to "resources/public/js/bundle/bundle.dev.js"
+                           :output-dir "resources/public/js/bundle"
+                           :optimizations :whitespace
+                           :pretty-print true
+                           :source-paths "resources/public/js/bundle/bundle.dev.js.map"}}
+               :pre
+               {:source-paths ["src/cljs"]
+                :compiler {:output-to "resources/public/js/bundle/bundle.pre.js"
+                           :optimizations :simple
+                           :pretty-print false}}
+               :prod
+               {:source-paths ["src/cljs"]
+                :compiler {:output-to "resources/public/js/bundle/bundle.prod.js"
+                           :optimizations :advanced
+                           :pretty-print false}}}}
+
+  :clean-targets ^{:protect false} [:target-path "resources/public/js/bundle/"]
+
+  :aliases {"serve" ["run" "-m" "server.core"]                      ; Server
+            "serve-repl" ["trampoline" "cljsbuild" "repl-listen"]
+            "head-start" ["run" "-m" "server.head.core"]            ; > Head
+            "client-rebuild-all" ["do" "clean," "cljsbuild" "once"] ; Client
+            "client-watch-all" ["cljsbuild" "auto"]
+            "client-build-dev" ["cljsbuild" "once" "dev"]
+            "client-watch-dev" ["cljsbuild" "auto" "dev"]
+            "client-build-pre" ["cljsbuild" "once" "pre"]
+            "client-watch-pre" ["cljsbuild" "auto" "pre"]
+            "client-build-prod" ["cljsbuild" "once" "prod"]
+            "client-watch-prod" ["cljsbuild" "auto" "prod"]}
 
   :repl-options {:init-ns server.core})
