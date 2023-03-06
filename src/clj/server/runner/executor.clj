e0b920954e1956e44adfa3927a3ed8b49a99eae4
shoegaze <bank.tellr@gmail.com>
2023-03-07 05:25:14 +0900

    Adds logs to executor

--- src/clj/server/runner/executor.clj
+++ src/clj/server/runner/executor.clj
@@ -1,4 +1,5 @@
-(ns server.runner.executor)
+(ns server.runner.executor
+  (:require [taoensso.timbre :as timbre]))
 
 
 (defn dirty? [_node-cache _node-graph]
@@ -6,12 +7,13 @@
 
 ; TODO: Pass step-context map
 (defn step [_node-cache _node-graph]
-  (taoensso.timbre/info "Stepping..."))
+  (timbre/info "Stepping..."))
 
 ; TODO: Pass execute-context map
 ; TODO: Return error code
 ; TODO: Start only when :start message is received
 (defn execute-loop [node-cache node-graph step-rate]
+  (timbre/info "Starting execute loop")
   (while true
     (when (dirty? node-cache node-graph)
       (step node-cache node-graph))
