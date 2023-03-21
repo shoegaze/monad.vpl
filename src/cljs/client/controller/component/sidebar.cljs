735d226e35e9852982e75472bc6892d368a287c8
shoegaze <bank.tellr@gmail.com>
2023-03-22 05:46:51 +0900

    FIX: corrects sidebar component to use updated edn schema

--- src/cljs/client/controller/component/sidebar.cljs
+++ src/cljs/client/controller/component/sidebar.cljs
@@ -2,29 +2,29 @@
   (:require [reagent.core :as r]))
 
 
-(defonce selected-node (r/atom nil))
-
-(defn sidebar [node-cache]
-  [:div.sidebar
-   [:h1.sidebar-title "Palette"]
-   [:div.sidebar-container
-    (when-let [cache (:cache @node-cache)]
-      (for [[id node] cache
-            :let [full-path (-> node :model :full-path)
-                  on-click  (fn []
-                              (reset! selected-node node))]]
-        ^{:key id} [:input.sidebar-entry {:type "button"
-                                          :value full-path
-                                          :on-click on-click}]))]
-   (when-let [node @selected-node]
-     (let [node-model (:model node)
-           {full-path :full-path
-            {:keys [tags description]} :meta} node-model]
-       [:div.sidebar-info
-        [:h1.sidebar-info-title full-path]
-        [:h2.sidebar-info-tags
-         (map-indexed
-           (fn [i tag]
-             ^{:key i} [:span.sidebar-info-tag tag])
-           tags)]
-        [:div.sidebar-info-description description]]))])
+(defn sidebar [_node-cache]
+  (let [selected-node (r/atom nil)]
+    (fn [node-cache]
+      [:div.sidebar
+       [:h1.sidebar-title "Palette"]
+       [:div.sidebar-container
+        (when-let [cache (:cache @node-cache)]
+          (for [[id node] cache
+                :let [full-path (-> node :model :full-path)
+                      on-click  (fn []
+                                  (reset! selected-node node))]]
+            ^{:key id} [:input.sidebar-entry {:type "button"
+                                              :value full-path
+                                              :on-click on-click}]))]
+       (when-let [node @selected-node]
+         (let [node-model (:model node)
+               {full-path :full-path
+                {:keys [tags description]} :metadata} node-model]
+           [:div.sidebar-info
+            [:h1.sidebar-info-title full-path]
+            [:h2.sidebar-info-tags
+             (map-indexed
+               (fn [i tag]
+                 ^{:key i} [:span.sidebar-info-tag tag])
+               tags)]
+            [:div.sidebar-info-description description]]))])))
