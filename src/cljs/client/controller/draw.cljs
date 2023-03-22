b2dfb23b92933364483addc7b70f1e450ecaa17c
shoegaze <bank.tellr@gmail.com>
2023-03-22 19:29:52 +0900

    Draws node instance debug pins

--- src/cljs/client/controller/draw.cljs
+++ src/cljs/client/controller/draw.cljs
@@ -27,13 +27,15 @@
          [sx sy] :scale} (-> instance :view :transform)
         {[w h] :size} (aabb instance)]
     (doto ctx
+      .beginPath
       (.translate dx dy)
       (.scale sx sy)
       (.rect 0 0 w h)
-      ;.fill
+      .fill
       .stroke
       (.setTransform 1 0 0
-                     1 0 0))))
+                     1 0 0)
+      .closePath)))
 
 (defn- draw-instance-icon [ctx instance node-cache]
   (set! (.-fillStyle ctx) "black")
@@ -51,31 +53,85 @@
          [sx sy] :scale} (-> instance :view :transform)
         {[w h] :size} (aabb instance)]
     (doto ctx
+      .beginPath
       (.translate dx dy)
       (.scale sx sy)
       (.fillText name (/ w 2) (/ h 2))
       (.setTransform 1 0 0
-                     1 0 0))))
+                     1 0 0)
+      .closePath)))
+
+(defn- draw-instance-input-pin [ctx instance input inputs]
+  (set! (.-fillStyle ctx) "aquamarine")
+  (set! (.-strokeStyle ctx) "black")
+
+  (let [{[dx dy] :translation
+         [sx sy] :scale} (-> instance :view :transform)
+        {[_ h] :size} (aabb instance)
+        r 10.0]
+    (doto ctx
+      .beginPath
+      (.translate dx dy)
+      (.scale sx sy)
+      (.ellipse 0 (/ h 2) r r 0.0 0.0 (* 2.0 js/Math.PI))
+      .fill
+      .stroke
+      (.setTransform 1 0 0
+                     1 0 0)
+      .closePath)))
+
+(defn- draw-instance-output-pin [ctx instance output outputs]
+  (set! (.-fillStyle ctx) "orangered")
+  (set! (.-strokeStyle ctx) "black")
+
+  (let [{[dx dy] :translation
+         [sx sy] :scale} (-> instance :view :transform)
+        {[w h] :size} (aabb instance)
+        r 10.0]
+    (doto ctx
+      .beginPath
+      (.translate dx dy)
+      (.scale sx sy)
+      (.ellipse w (/ h 2) r r 0.0 0.0 (* 2.0 js/Math.PI))
+      .fill
+      .stroke
+      (.setTransform 1 0 0
+                     1 0 0)
+      .closePath)))
+
+(defn- draw-instance-pins [ctx instance node-cache]
+  (let [{inputs  :inputs
+         outputs :outputs} (->> instance
+                                :full-path
+                                (get-node node-cache)
+                                :model
+                                :metadata)]
+    (doseq [input inputs]
+      (draw-instance-input-pin ctx instance input inputs))
+
+    (doseq [output outputs]
+      (draw-instance-output-pin ctx instance output outputs))))
 
 (defn- draw-instance [ctx instance node-cache]
   (draw-instance-frame ctx instance)
-  ; TODO: Draw icon on top of frame
+  ; TODO: Draw on top of frame
   (draw-instance-icon ctx instance node-cache)
-  ; TODO:
-  ;(draw-instance-pins ctx instance)
+  (draw-instance-pins ctx instance node-cache)
   )
 
-(defn- draw-instances [ctx instances node-cache]
-  (doseq [instance instances]
-    (draw-instance ctx instance node-cache)))
+(defn- draw-instances [ctx node-cache node-graph]
+  (let [instances (:instances node-graph)]
+    (doseq [instance instances]
+      (draw-instance ctx instance node-cache))))
 
 
 (defn- draw-edge [ctx edge]
   :TODO)
 
 (defn- draw-edges [ctx edges]
-  (doseq [edge edges]
-    (draw-edge ctx edge)))
+  (let [edges (:edges edges)]
+    (doseq [edge edges]
+      (draw-edge ctx edge))))
 
 
 (defn draw [node-cache node-graph ctx]
@@ -83,8 +139,9 @@
 
   (clear ctx)
 
-  (let [{instances :instances
-         edges     :edges} @node-graph]
-    (draw-instances ctx instances @node-cache)
-    ;(draw-edges     ctx edges)
+  ; TODO: Rename atoms with trailing _
+  (let [node-cache @node-cache
+        node-graph @node-graph]
+    (draw-instances ctx node-cache node-graph)
+    ;(draw-edges     ctx node-graph)
     ))
